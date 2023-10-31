c924859db2d31f752df19f39f10d34372f6daceb
NiK <a.s7eve777@gmail.com>
2023-10-31 22:25:17 +0500

    Completed authentication

--- KeyboardsMarkup.py
+++ KeyboardsMarkup.py
@@ -2,13 +2,9 @@ from telebot import types
 
 # Start
 markup_start = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-subject1 = types.KeyboardButton('1️⃣')
-subject2 = types.KeyboardButton('2️⃣')
-subject3 = types.KeyboardButton('3️⃣')
-subject4 = types.KeyboardButton('4️⃣')
-subject5 = types.KeyboardButton('5️⃣')
-hastory = types.KeyboardButton('История (Конфуцианство в Китае)')
-markup_start.add(subject1, subject2, subject3, subject4, subject5, hastory)
+name = types.KeyboardButton('name')
+
+markup_start.add(name)
 
 
 # Send date
@@ -16,11 +12,6 @@ markup_start.add(subject1, subject2, subject3, subject4, subject5, hastory)
 markup_send_nummer = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
 nummer = types.KeyboardButton(text='Send phone', request_contact=True)
 markup_send_nummer.add(nummer)
-
-# send geolocation
-markup_send_geolocation = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-geolocation = types.KeyboardButton(text='Send geolocation', request_location=True)
-markup_send_geolocation.add(geolocation)
 
 # -=-=-=-=-=-=-=-=-=- Admin Panel -=-=-=-=-=-=-=-=-=- #
 
