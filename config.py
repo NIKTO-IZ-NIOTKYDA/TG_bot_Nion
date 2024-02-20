e0fa5a3eb39c6e0fc075fd85768600b64db4670b
NiK <a.s7eve777@gmail.com>
2024-02-20 21:32:25 +0500

    Release 1.2.1 [stable]

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BotToken = '6708933796:AAGMTGwn-wSgXLI24fWQe0Jow-xCvfvioCk'  # Токен бота
+BotToken = '6552240356:AAFsALP8O1pEIXXEPVPRsIwByDbKX6oRnf8'  # Токен бота
 commands_admin = 'AdminPanel_4qB7cY9jZ2gP'  # Команда для доступа к админ панели
 clear_konsole = 'clear'
 name_database = 'db/db.db'
@@ -6,7 +6,7 @@ reboot_command = 'reboot -f'
 shutdown_command = 'systemctl disable TG_bot_Nion.service && shutdown now'
 version = 'Release 1.2.1 [stable]'  # X.X.X [stable\beta\dev]
 log = True
-debug = True
+debug = False
 error = False
 welcome_animation = False
 name_log_file = 'log.log'
