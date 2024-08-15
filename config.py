fa0f8e4dc92505cbe1f1855824ccf2a1d659acfa
NIKTO-IZ-NEOTKYDA <a.s7eve777@gmail.com>
2024-08-15 09:46:14 +0500

    Release 1.4.1 [stable]

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-BotToken: str = r'6552240356:AAHmdQHQ9Y0p84G-zFNo_Ho1D6TPdoOMtJQ'  # Токен бота 6552240356:AAHmdQHQ9Y0p84G-zFNo_Ho1D6TPdoOMtJQ
+BotToken: str = r'6708933796:AAFQ8sc4n3VRCXpCWzxemg-rkVP-NVJxsOA'  # Токен бота 6552240356:AAHmdQHQ9Y0p84G-zFNo_Ho1D6TPdoOMtJQ
 commands_admin: str = r'AdminPanel_4qB7cY9jZ2gP'  # Команда для доступа к админ панели
 clear_konsole: str = r'clear'
 name_database: str = r'db/db.db'
-version: str = r'Release 1.4.1 [stable]'  # X.X.X [stable\beta\dev]
+version: str = r'Release 2.0.0 [dev]'  # X.X.X [stable\beta\dev]
 log: bool = True
 debug: bool = True
 welcome_animation: bool = False
